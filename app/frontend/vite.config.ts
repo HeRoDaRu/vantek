b8d1a949755919b764c5de28b2369150e8c130e6
Héctor Rodríguez <hectoor.16@icloud.com>
2026-05-09 18:34:52 +0200

    Actualizar vite.config.ts

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -1,19 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: './dist',
-    emptyOutDir: true,
+  resolve: {
+    alias: { '@': resolve(__dirname, './src') }
   },
   server: {
     port: 5173,
     proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-      },
-    },
+      '/api': { target: 'http://localhost:3000', changeOrigin: true }
+    }
   },
-});
+  build: {
+    outDir: '../../dist/public',
+    emptyOutDir: true
+  }
+})
